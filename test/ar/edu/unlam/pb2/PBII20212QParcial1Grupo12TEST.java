71fd85269a23aa3630ae077b5114ea2e1a80e955
cpinaud <cintiapinaud@gmail.com>
2021-10-13 21:46:41 -0300

    Creacion del proyecto

---
+++ test/ar/edu/unlam/pb2/PBII20212QParcial1Grupo12TEST.java
@@ -0,0 +1,14 @@
+package ar.edu.unlam.pb2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PBII20212QParcial1Grupo12TEST {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
